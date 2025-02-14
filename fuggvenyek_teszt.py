7fc6a8960d96f1199146fff9b180d5a77a160550
Barni <tothbarni.06@gmail.com>
2025-02-14 14:22:37 +0100

    coloramaSzinezesNelkul

--- fuggvenyek_teszt.py
+++ fuggvenyek_teszt.py
@@ -1,23 +1,32 @@
 import fuggvenyek
-from colorama import Fore, Style
+
 import os
 os.system('CLS')
+
+Y = "\033[33m"
+B = "\033[1m"
+C = "\033[36m"
+G = "\033[32m"
+R = "\033[31m"
+X = "\033[0m"
 
 def sorta_sum_teszt():
     a_lista = [3, 9, 10, 12, -3, 4, 4, 14, 14]
     b_lista = [4, 4, 11, -3, 12, 5, 6, 7, 6]
     vart_lista = [7, 20, 21, 9, 9, 9, 20, 21, 20]
 
-    for i in range(0, len(a_lista), 1):
-        print(f"{Fore.YELLOW}{Style.BRIGHT}{i+1}. teszteset:{Style.RESET_ALL}")
-        a:int=a_lista[i]
-        b:bool=b_lista[i]
-        vart:bool=vart_lista[i]
-        kapott:bool=fuggvenyek.sorta_sum(a, b)
-        if (vart==kapott):
-            eredmeny=f"{Style.BRIGHT}{Fore.GREEN}OK{Fore.RESET}"
+    for i in range(len(a_lista)):
+        print(f"{Y}{B}{i+1}. teszteset:{X}")
+        a = a_lista[i]
+        b = b_lista[i]
+        vart = vart_lista[i]
+        kapott = fuggvenyek.sorta_sum(a, b)
+
+        if vart == kapott:
+            eredmeny = f"{B}{G}OK{X}"
         else:
-            eredmeny=f"{Style.BRIGHT}{Fore.RED}HIBA{Fore.RESET}"
-        print(f"{Fore.CYAN}a: {a} | b: {b} |{Fore.RESET} várt összeg: {vart} | kapott: {kapott} | eredmény: {eredmeny}\n")
+            eredmeny = f"{B}{R}HIBA{X}"
+
+        print(f"{C}a: {a} | b: {b} |{X} várt összeg: {vart} | kapott: {kapott} | eredmény: {eredmeny}\n")
 
 sorta_sum_teszt()
